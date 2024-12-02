dc40532db0846499410ba41e51659b21ccd73714
60236833 <60236833@leroymerlin.ru>
2024-12-02 13:09:34 +0300

    - Added file input instead of console input - Refactored project structure - Fixed the second part of Day 2's puzzle - Added README file

--- day_2/helper.go
+++ day_2/helper.go
@@ -11,18 +11,22 @@ import (
 func Calculate() {
 	var reports [][]int
 	ReadLinesFromStdinToSlice(&reports)
+	fmt.Println("Day 2!")
 
-	// Day 2 Task 1
+	// Part 1
 	totalSafeReports := CalculateSafeReports(reports)
 	fmt.Printf("Total Distance: %d\n", totalSafeReports)
-	// Day 2 Task 2
+	// Part 2
 	totalSafeReportsWithDampener := CalculateSafeReportsWithDampener(reports)
-	fmt.Printf("Total Distance: %d\n", totalSafeReportsWithDampener)
+	fmt.Printf("Total Distance with Dampener: %d\n\n", totalSafeReportsWithDampener)
 }
 
 func ReadLinesFromStdinToSlice(lines *[][]int) {
-	fmt.Println("Day 2. Input:")
-	scanner := bufio.NewScanner(os.Stdin)
+	file, _ := os.Open("day_2/input.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
 	for scanner.Scan() {
 		line := scanner.Text()
 		if line == "" {
